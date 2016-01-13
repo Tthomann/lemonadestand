e04e1c384e988fc8a02b7deec301de3c4f89eb20
Tyler Thomann <Tyler Thomann>
2016-01-13 15:15:55 -0600

    weathersystems complete, buy functions

--- lemon.js
+++ lemon.js
@@ -1,41 +1,116 @@
 "use strict"
-
-function weather(){
+function DaySelection7(){
+	for(daysleft=7;daysleft>0;daysleft-1);
+}
+function DaySelection14(){
+	for(daysleft=14;daysleft>0;daysleft-1);
+}
+function DaySelection21(){
+	for(daysleft=21;daysleft>0;daysleft-1);
+}
+function WeatherGenerator(){
 
 var randomnumber;
-var weathersystem;
+var weatherstatus;
 
-randomnumber = Math.floor(Math.random() * (100 - 0 + 1)) + 0;
+randomnumber = Math.floor(Math.random() * (100 - 1 + 1)) + 0;
 
 	if (randomnumber <=25){
 		console.log ("sunny");
-    weathersystem = "sunny";
-  }
+    weatherstatus = "sunny";
+  	}
 	else if (randomnumber>25 && randomnumber <55){
 		console.log ("rain");
-    weathersystem = "rain";
+    weatherstatus = "rain";
 	}
 	else if (randomnumber>55 && randomnumber <80){
 		console.log ("cloud");
-    weathersystem = "cloud";
+    weatherstatus = "cloud";
 	}
 	else if (randomnumber>80 && randomnumber <100){
 		console.log ("storm");
-    weathersystem = "storm";
+    weatherstatus = "storm";
 	}
 		return randomnumber;
  }
- var weathernumber;
-	weathernumber=weather();
+ var weatherstatus;
+	weatherstatus=WeatherGenerator();
 
-function condintionalsale(){
+function TemperatureGenerator(){
+	var randomnumber;
+	var temperaturestatus;
 
-	if weathersystem = "sunny";
-		modifysale = 1;
-	else if weathersystem = "rain";
-		modifysale = .5;
-	else if weathersystem = "cloud";
-		modifysale = .7;
-	else if weathersystem = "storm";
-		modifysale = 0;
+randomnumber = Math.floor(Math.random() * (100 - 1 + 1)) + 0;
+temperaturestatus;
+	if (randomnumber <=25){
+		console.log ("It's cold outside!");
+    temperaturestatus = "cold";
+  	}
+	else if (randomnumber>25 && randomnumber <=50){
+		console.log ("It's warm outside!");
+   temperaturestatus = "warm";
+	}
+	else if (randomnumber>55 && randomnumber <=75){
+		console.log ("It's hot outside!");
+    temperaturestatus = "hot";
+	}
+	else if (randomnumber>75 && randomnumber <=100){
+		console.log ("It's VERY hot outside!");
+    temperaturestatus = "VERY hot";
+	}
+		return randomnumber;
+ }
+  var temperaturestatus;
+	temperaturestatus=TemperatureGenerator();
+
+function WeatherPrediction(){
+	var forecast
+	var random
+		forecast = ["bad","okay","great"];
+		random = forecast[Math.floor(Math.random() *forecast.length)];
+		console.log(random);
+		return forecast;
 }
+WeatherPrediction();
+
+function CupsBought(){
+	var numberofcups;
+	numberofcups = prompt("enter # of cups to buy"); 
+		return numberofcups;
+}
+CupsBought();
+
+function LemonsBought(){
+	var numberoflemons;
+	numberoflemons = prompt("enter # of lemons to buy"); 
+		return numberoflemons;
+}
+LemonsBought();
+
+function SugarBought(){
+	var numberofsugar;
+	numberofsugar = prompt("enter # of sugar to buy"); 
+		return numberofsugar;
+}
+SugarBought();
+
+function IceBought(){
+	var numberofice;
+	numberofice = prompt("enter # of ice to buy"); 
+		return numberofice;
+}
+IceBought();
+
+function CupsToMake(){
+	var cups;
+	cups = prompt("enter # of cups to make")
+		return cups;
+}
+CupsToMake();
+
+function PitchersToMake(){
+	var pitchers;
+	pitchers = prompt("enter # of pitchers to make")
+		return pitchers;
+}
+PitchersToMake();
